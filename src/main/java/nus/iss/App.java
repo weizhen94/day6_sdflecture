23c9fc26af2e8511cd5077ff78305662dd0b1559
weizhen94 <weizhen94@gmail.com>
2023-01-17 10:35:06 +0800

    first commit

---
+++ src/main/java/nus/iss/App.java
@@ -0,0 +1,13 @@
+package nus.iss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
